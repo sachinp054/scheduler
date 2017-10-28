58f5053a68e9ca78093795f493a99de56a4c63f4
Sachin Pandey <sachinp054@gmail.com>
2017-10-28 16:50:17 +0530

    Exception categories

---
+++ src/main/java/com/sacknibbles/sch/scheduler/exception/InvalidCronExpression.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sacknibbles.sch.scheduler.exception;
+
+/**
+ * @author Sachin
+ *
+ */
+public class InvalidCronExpression extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidCronExpression(String message){
+		super(message);
+	}
+}
---
+++ src/main/java/com/sacknibbles/sch/scheduler/exception/UnSupportedOperationException.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sacknibbles.sch.scheduler.exception;
+
+/**
+ * @author Sachin
+ *
+ */
+public class UnSupportedOperationException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnSupportedOperationException(String message){
+		super(message);
+	}
+}
